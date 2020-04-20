adc4dfdd7e52a4d38cd993781f45b56400d3f6e0
Jonatandb <jonatandb@gmail.com>
2020-04-19 22:31:28 -0300

    Agrego código base que dibuja la grilla

--- JuegoDeLaVida.py
+++ JuegoDeLaVida.py
@@ -3,3 +3,43 @@ import numpy as np
 
 pygame.init()
 
+# Ancho y alto de la pantalla
+width, height = 1000, 1000
+
+# Creación de la pantalla
+screen = pygame.display.set_mode((height, width))
+
+# Color de fondo, casi negro
+bg = 25, 25, 25
+
+# Pinto el fondo con el color elegido (bg)
+screen.fill(bg)
+
+# Cantidad de celdas en cada eje
+nxC, nyC = 25, 25
+
+# Ancho y alto de cada celda
+dimCW = width / nxC
+dimCH = height / nyC
+
+# Bucle de ejecución principal (Main Loop)
+while True:
+
+    # Recorro cada una de las celdas generadas
+    for y in range(0, nxC):
+        for x in range(0, nyC):
+
+            poly = [
+                ((x) * dimCW, y * dimCH),
+                ((x + 1) * dimCW, y * dimCH),
+                ((x + 1) * dimCW, (y + 1) * dimCH),
+                ((x) * dimCW, (y + 1) * dimCH),
+            ]
+
+            # screen          -> Pantalla donde dibujar
+            # (128, 128, 128) -> Color a utilizar para dibujar, en este caso un gris
+            # poly            -> Puntos que definan al poligono que se está dibujando
+            pygame.draw.polygon(screen, (128, 128, 128), poly, 1)
+
+    # Muestro y actualizo los fotogramas en cada iteración del bucle principal
+    pygame.display.flip()
--- Readme.md
+++ Readme.md
@@ -4,8 +4,12 @@
 
 # <a href="https://www.youtube.com/watch?v=qPtKv9fSHZY" target="_blank">https://www.youtube.com/watch?v=qPtKv9fSHZY</a>
 
-#
-
 Configuro VSCode para que no muestre un alerta erronea sobre pygame
 
-https://stackoverflow.com/questions/50569453/why-does-it-say-that-module-pygame-has-no-init-member
+<a href="https://stackoverflow.com/questions/50569453/why-does-it-say-that-module-pygame-has-no-init-member" target="_blank">Why does it say that module pygame has no init member?</a>
+
+Configuro VSCode para que entorno de debugging por defecto sea el que ejecuta el archivo actual y para que también formatee el código usando black luego de investigar las opciones disponibles (Black, yapf y autopep8, finalmente instalé Black) y leer éstas páginas:
+
+<a href="https://www.reddit.com/r/Python/comments/8oqy03/blog_a_comparison_of_autopep8_black_and_yapf_code/" target="_blank">Blog: A comparison of autopep8, black, and yapf - Code formatters for Python</a>
+
+<a href="https://medium.com/3yourmind/auto-formatters-for-python-8925065f9505" target="_blank">Auto formatters for Python 👨‍💻🤖</a>
